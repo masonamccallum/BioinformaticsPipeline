d03cc566c2eb8645c5ac59b632b478708c7b91d3
masonamccallum <masonamccallum@gmail.com>
2020-07-16 16:17:39 -0500

    updated logging

--- pipeline.bash
+++ pipeline.bash
@@ -28,16 +28,16 @@ mkdir -p $data/out
 if [ ! -d $data ]; then
 	echo "Data directory is invalid"
 	exit
-elif [ ! -f $data'/index1.fastq' ]; then
+elif [ ! -f $data'/raw/index1.fastq' ]; then
 	echo "index1.fastq not found"	
 	exit
-elif [ ! -f $data'/index2.fastq' ]; then
+elif [ ! -f $data'/raw/index2.fastq' ]; then
 	echo "index2.fastq not found"	
 	exit
-elif [ ! -f $data'/read1.fastq' ]; then
+elif [ ! -f $data'/raw/read1.fastq' ]; then
 	echo "read1.fastq not found"	
 	exit
-elif [ ! -f $data'/read2.fastq' ]; then
+elif [ ! -f $data'/raw/read2.fastq' ]; then
 	echo "read2.fastq not found"	
 	exit
 elif [ ! -f $data'/mappingData.txt' ]; then
@@ -67,6 +67,10 @@ echo '2: 16s'
 
 read section
 
+logfile= $data/out/out.log
+touch logfile
+echo $(date "+%d-%m-%y") > logfile
+
 case $section in
 	1)
 		echo "ITS"
@@ -93,8 +97,8 @@ echo -ne "\e[0m"
 
 	rm -f $data/out/*
 	# join index files to indices file
-	$usearch -fastq_join $data/index1.fastq -reverse $data/index2.fastq -join_padgap\
-		"" -threads 1 -fastqout $data/out/indices.fastq
+	$usearch -fastq_join $data/raw/index1.fastq -reverse $data/raw/index2.fastq -join_padgap\
+		"" -threads 1 -fastqout $data/out/indices.fastq | tee -ai logfile
 
 	# createBarcode file
 	while IFS=$'\t' read -r -a line
@@ -111,72 +115,72 @@ fi
 if [ $choice -le 2 ]
 then
 echo -ne "\e[97m"
-echo "===================================================================================="
-echo "=                         Demultiplex                                              ="
-echo "===================================================================================="
+echo "====================================================================================" | tee -ai logfile
+echo "=                         Demultiplex                                              =" | tee -ai logfile
+echo "====================================================================================" | tee -ai logfile
 echo -ne "\e[0m"
-	$usearch -fastx_demux $data/read1.fastq -reverse $data/read2.fastq\
+	$usearch -fastx_demux $data/raw/read1.fastq -reverse $data/raw/read2.fastq\
 		-index $data/out/indices.fastq -barcodes $data/out/bar.fasta\
-		-fastqout $data/out/demux_R1.fastq -output2 $data/out/demux_R2.fastq
-	$usearch -fastx_info $data/out/demux_R1.fastq
+		-fastqout $data/out/demux_R1.fastq -output2 $data/out/demux_R2.fastq | tee -ai logfile
+	$usearch -fastx_info $data/out/demux_R1.fastq | tee -ai logfile
 fi
 
 
 if [ $choice -le 3 ]
 then
 echo -ne "\e[97m"
-echo "===================================================================================="
-echo "=                         Merge Pairs                                              ="
-echo "===================================================================================="
+echo "====================================================================================" | tee -ai logfile
+echo "=                         Data Prep                                                =" | tee -ai logfile
+echo "====================================================================================" | tee -ai logfile
 echo -ne "\e[0m"
 	$usearch -fastq_mergepairs $data/out/demux_R1.fastq \
-		-fastq_maxdiffs 5 -fastqout $data/out/merged.fastq
+		-fastq_maxdiffs 5 -fastqout $data/out/merged.fastq | tee -ai logfile
 
 	$usearch -fastq_filter $data/out/merged.fastq -fastq_maxee 1.0\
-		-fastaout $data/out/filtered.fasta 
+		-fastaout $data/out/filtered.fasta  | tee -ai logfile
 
-	$usearch8 -search_pcr $data/out/filtered.fasta -db $data/primer16s.fasta -strand both \
-		-maxdiffs 3 -minamp 225 -maxamp 325 -pcr_strip_primers -ampout $data/out/filtered.fasta
+	$usearch8 -search_pcr $data/out/filtered.fasta -db $data/refData/primer16s.fasta -strand both \
+		-maxdiffs 3 -minamp 225 -maxamp 325 -pcr_strip_primers -ampout $data/out/filtered16s.fasta | tee -ai logfile
 	
 	#$usearch -fastx_truncate $data/out/merged.fastq -stripleft 19 -stripright 20 \
 	#	-fastqout $data/out/strippedMerged.fastq
 
-	$usearch -fastx_uniques $data/out/filtered.fasta -fastaout $data/out/uniques.fasta\
-	-sizeout -relabel Uniq 
+	$usearch -fastx_uniques $data/out/filtered16s.fasta -fastaout $data/out/uniques.fasta\
+	-sizeout -relabel Uniq  | tee -ai logfile
 fi
 
 if [ $choice -le 4 ]
 then
 echo -ne "\e[97m"
-echo "===================================================================================="
-echo "=                         Create OTU                                               ="
-echo "===================================================================================="
+echo "====================================================================================" | tee -ai logfile
+echo "=                         Create OTU/ZOTU/Table                                    =" | tee -ai logfile
+echo "====================================================================================" | tee -ai logfile
 echo -ne "\e[0m"
 	$usearch -cluster_otus $data/out/uniques.fasta -minsize 2 \
-		-otus $data/out/otus.fasta -relabel Otu
+		-otus $data/out/otus.fasta -relabel Otu | tee -ai logfile
 
 	$usearch -unoise3 $data/out/uniques.fasta -zotus $data/out/zotu.fasta\
-		-tabbedout $data/out/unoise3.txt
+		-tabbedout $data/out/unoise3.txt | tee -ai logfile
 	
 	$usearch -otutab $data/out/merged.fastq -otus $data/out/otus.fasta -otutabout \
-		$data/out/otutab.txt
+		$data/out/otutab.txt | tee -ai logfile
 fi
 
 if [ $choice -le 5 ]
 then 
 echo -ne "\e[97m"
-echo "===================================================================================="
-echo "=                         Taxonomy Prediction                                      ="
-echo "===================================================================================="
+echo "====================================================================================" | tee -ai logfile
+echo "=                         Taxonomy Prediction                                      =" | tee -ai logfile
+echo "====================================================================================" | tee -ai logfile
 echo -ne "\e[0m"
 	$usearch -sintax $data/out/otus.fasta -db $data/refData/rdp_16s_v16.fa \
-		-tabbedout $data/out/reads.sintax -strand both -sintax_cutoff 0.8
+		-tabbedout $data/out/reads.sintax -strand both -sintax_cutoff 0.8 | tee -ai logfile
 	
 	# python sintax file correction
-	python scripts/fixSintax.py $data/out/reads.sintax
+	python scripts/fixSintax.py $data/out/reads.sintax | tee -ai logfile
 
 	$usearch -sintax_summary $data/out/reads.sintax -otutabin $data/out/otutab.txt \
-		-output	$data/out/phylum_summary.txt -rank g
+		-output	$data/out/phylum_summary.txt -rank g | tee -ai logfile
 
 fi
 
