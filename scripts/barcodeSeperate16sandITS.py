7b4281f8561eb1a25142cad1a3528508c7460578
Mason McCallum <33843826+masonamccallum@users.noreply.github.com>
2021-02-11 19:22:14 -0600

    Update barcodeSeperate16sandITS.py

--- scripts/barcodeSeperate16sandITS.py
+++ scripts/barcodeSeperate16sandITS.py
@@ -6,6 +6,7 @@ barFile = sys.argv[1]
 seqs = []
 bar16S = open("out/16Sbar.fasta","w")
 barITS = open("out/ITSbar.fasta","w")
+bar18S = open("out/18Sbar.fasta","w")
 
 with open(barFile,'r') as barIn:
 	dataList = barIn.readlines()
@@ -17,9 +18,13 @@ with open(barFile,'r') as barIn:
 		if "ITS" in seq['ID']:   #If this is not specified sequences are assumed to be 16s
 			barITS.write(seq['ID']+'\n')
 			barITS.write(seq['barcode']+'\n')
+		if "18S" in seq['ID']:
+			bar18S.write(seq['ID']+'\n')
+			bar18S.write(seq['barcode']+'\n')
 		else:
 			bar16S.write(seq['ID']+'\n')
 			bar16S.write(seq['barcode']+'\n')
 
 barITS.close()
 bar16S.close()
+bar18S.close()
